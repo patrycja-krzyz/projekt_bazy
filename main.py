7e0959a6a6c6cc5c5788dece9c4cca2f2daadd3a
Patrycja Krzyżostanek <patrycja.krzyzostanek@gmail.com>
2026-01-20 03:35:16 +0100

    szkielet

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
-# pierwszy sposób
-from budowa import Baza
-import sqlite3
+import mysql.connector
 
-park = Baza()
-
-# do wypróbowania
-park.cursor.execute(
-    "INSERT INTO attractions (attraction_name, built_date) VALUES (?, ?)",
-    ("Rollercoaster", "2009-11-21")
+con = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "haslo_maslo67",
+    database = "AiBtcQuantLandia"
 )
-park.cursor.execute("SELECT * FROM attractions")
-wyniki = park.cursor.fetchall()
-for wiersz in wyniki:
-    print(wiersz)
 
-# park.zamknij()
+cursor = con.cursor()
 
+#tworzenie tabel
+with open("budowa1.sql", "r", encoding="utf-8") as f:
+    komendy = f.read()
+for komenda in komendy.split(';'): #rozdzielam komendy
+    komenda = komenda.strip()
+    if komendy:
+        cursor.execute(komenda)
 
-
-# #drugi sposób - wybierzcie sobie który
-# import sqlite3
-
-# con = sqlite3.connect("park.db") #jeśli baza nie istnieje, to ją utworzy w pracującym folderze
-# cursor = con.cursor()
-
-# with open("budowa2.sql", "r", encoding="utf-8") as f:
-#     cursor.executescript(f.read())
-
-# #do wypróbowania
-# cursor.execute(
-#     "INSERT INTO attractions (attraction_name, built_date) VALUES (?, ?)",
-#     ("Rollercoaster", "2009-11-21")
-# )
-# cursor.execute("SELECT * FROM attractions")
-# wyniki = cursor.fetchall()
-# for wiersz in wyniki:
-#     print(wiersz)
-
-
-# con.commit()
-# cursor.close()
-# con.close()
+con.commit()
--- pierwsza_tabela.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE pracownicy (
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-imię_i_nazwisko VARCHAR(50),
-płeć ENUM('kobieta', 'mężczyzna') NOT NULL
-);
