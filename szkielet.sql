709f78b7f663206cf2b6e96d43455d7f91f60bfe
Patrycja Krzyżostanek <patrycja.krzyzostanek@gmail.com>
2026-01-21 13:09:18 +0100

    Update szkielet.sql

--- szkielet.sql
+++ szkielet.sql
@@ -14,25 +14,26 @@ CREATE TABLE IF NOT EXISTS inspections (
 
 CREATE TABLE IF NOT EXISTS malfunctions (
     malfunction_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    attraction_id INT UNSIGNED NOT NULL,
-    accident_date DATETIME NOT NULL,
+    attraction_id INT UNSIGNED,
+    accident_date DATETIME,
     fix_date DATETIME,
     comment VARCHAR(400),
+    fix_cost DECIMAL(8,2),
     FOREIGN KEY (attraction_id) REFERENCES attractions(attraction_id)
 );
 
-CREATE TABLE IF NOT EXISTS guest (
+CREATE TABLE IF NOT EXISTS guests (
     guest_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     birth_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS visit (
+CREATE TABLE IF NOT EXISTS visits (
     visit_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    guest_id INT UNSIGNED NOT NULL,
-    visit_date DATETIME NOT NULL,
-    FOREIGN KEY (guest_id) REFERENCES guest(guest_id)
+    guest_id INT UNSIGNED,
+    visit_date DATETIME,
+    FOREIGN KEY (guest_id) REFERENCES guests(guest_id)
 );
 
 CREATE TABLE IF NOT EXISTS incident_type (
@@ -49,10 +50,10 @@ CREATE TABLE IF NOT EXISTS guest_insurance (
     guest_id INT UNSIGNED,
     insurance_id INT UNSIGNED,
     PRIMARY KEY (guest_id, insurance_id),
-    FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
+    FOREIGN KEY (guest_id) REFERENCES guests(guest_id),
     FOREIGN KEY (insurance_id) REFERENCES insurance(insurance_id)
 );
-CREATE TABLE IF NOT EXISTS incident (
+CREATE TABLE IF NOT EXISTS incidents (
     incident_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     incident_type_id INT UNSIGNED NOT NULL,
     guest_id INT UNSIGNED NOT NULL,
@@ -60,7 +61,7 @@ CREATE TABLE IF NOT EXISTS incident (
     incident_date DATETIME,
     description VARCHAR(400),
     FOREIGN KEY (incident_type_id) REFERENCES incident_type(incident_type_id),
-    FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
+    FOREIGN KEY (guest_id) REFERENCES guests(guest_id),
     FOREIGN KEY (attraction_id) REFERENCES attractions(attraction_id)
 );
 
@@ -68,15 +69,17 @@ CREATE TABLE IF NOT EXISTS costs (
     cost_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     cost_type VARCHAR(50),
     amount DECIMAL(10,2),
-    how_often VARCHAR(50)
+    how_often VARCHAR(50),
+    attraction_id INT UNSIGNED,
+    FOREIGN KEY (attraction_id) REFERENCES attractions(attraction_id)
 );
 
 CREATE TABLE IF NOT EXISTS payments (
     payment_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     payment_date DATETIME,
-    amount DECIMAL(10,2),
-    income BOOLEAN NOT NULL, 
-    description VARCHAR(200)
+    amount DECIMAL(5,2),
+    guest_id INT UNSIGNED,
+    FOREIGN KEY (guest_id) REFERENCES guests(guest_id)
 );
 
 CREATE TABLE IF NOT EXISTS employees (
@@ -85,11 +88,22 @@ CREATE TABLE IF NOT EXISTS employees (
     last_name VARCHAR(50),
     position VARCHAR(50),
     salary DECIMAL(6,2),
-    hire_date DATE
+    hire_date DATE,
+    attraction_id INT UNSIGNED,
+    FOREIGN KEY (attraction_id) REFERENCES attractions(attraction_id)
 );
 
-CREATE TABLE IF NOT EXISTS tickets (
+CREATE TABLE IF NOT EXISTS prices (
     ticket_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     amount DECIMAL(4,2),
-    ticket_type VARCHAR(50)
+    attraction_id INT UNSIGNED,
+    FOREIGN KEY (attraction_id) REFERENCES attractions(attraction_id)
+);
+
+CREATE TABLE IF NOT EXISTS payment_ticket (
+    payment_id INT UNSIGNED NOT NULL,
+    ticket_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (payment_id, ticket_id),
+    FOREIGN KEY (payment_id) REFERENCES payments(payment_id),
+    FOREIGN KEY (ticket_id) REFERENCES prices(ticket_id)
 );
