d555ea637b64af17f419dd0a9a1b7637c3d103f2
emiliapawlak <287335@student.pwr.edu.pl>
2026-02-01 14:38:43 +0100

    reszta analizy i mała zmiana w payments

--- generators/payments.py
+++ generators/payments.py
@@ -45,7 +45,7 @@ def generate_payments(baza, payments_tickets, open_hour, closure_hour) -> pd.Dat
     lst = payments_tickets.groupby("payment_id")["ticket_id"].apply(list)
     prices_df = pd.read_sql("SELECT * FROM prices", con=baza.con)
     price_dict = prices_df.set_index("ticket_id")["amount"].to_dict()
-    dates_to_gen = pd.date_range("2026-01-01", "2026-01-31")        #mamy info za ostatni miesiac
+    dates_to_gen = pd.date_range("2025-01-01", "2026-01-01")
     for ls in lst:
         total = 0
         for ticket in ls:
